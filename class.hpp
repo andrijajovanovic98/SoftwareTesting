cf1d3cf34407c487d6069db7978fc18551d408df
Andrija Jovanovic <144850132+andrijajovanovic98@users.noreply.github.com>
2024-10-18 21:08:07 +0200

    Create class.hpp

---
+++ class.hpp
@@ -0,0 +1,16 @@
+#ifndef CLASS_HPP
+#define CLASS_HPP
+
+class Calculator {
+public:
+
+    static int add(int a, int b) {
+        return (a + b);
+    }
+    static int substract(int a, int b) {
+        return (a - b);
+    }
+
+};
+
+#endif
