d455f8a6cbc0f5c281c67d92ec7468c1dc5691f8
Andrija Jovanovic <144850132+andrijajovanovic98@users.noreply.github.com>
2024-10-18 21:07:43 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "class.hpp"
+#include <gtest/gtest.h>
+
+TEST(CalculatorTest, AddFunction) {
+    EXPECT_EQ(5, Calculator::add(2,3));
+    EXPECT_EQ(6, Calculator::add(2,4));
+    EXPECT_EQ(10, Calculator::add(7,3));
+}
+
+TEST(CalculatorTest, substractFunction) {
+    EXPECT_EQ(-1, Calculator::substract(2,3));
+    EXPECT_EQ(-2, Calculator::substract(2,4));
+    EXPECT_EQ(4, Calculator::substract(7,3));
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
